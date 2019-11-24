af89e09def1b30c20c235d21e950b612f75411b1
Luke Feeney <luke@localhost.localdomain>
2019-11-24 18:04:53 +0000

    intial

---
+++ scripts/AA_Group_Project.R
@@ -0,0 +1,41 @@
+# Title: "Advanced Analytics Group Project Assignment 2"
+# Authors: Gavin Byrne (A00267975), Luke Feeney (A00268130),Brendan Lynch (A00267986)
+# Date: Nov 2019
+
+# Install Libraries used if they are not already installed
+if("tidyr" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("tidyr")
+}
+if("tibble" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("tibble")
+}
+if("dplyr" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("dplyr")
+}
+if("stats" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("stats")
+}
+if("kableExtra" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("kableExtra")
+}
+if("knitr" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("knitr")
+}
+
+# Load libraries used in the project
+library(dplyr)
+library(tidyr)
+library(kableExtra)
+library(knitr)
+
+
+# Section 1 - Data Loading, Cleaning & Exploration ###############################
+
+
+# Section 2 - Decision Trees ###############################
+
+
+# Section 3 - kNN ###############################
+
+
+# Section 4 - Regression ###############################
